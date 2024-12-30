441aeec7e06e4fbf92722a33619637ed0aa424b2
Jakub Skowron <jakubskowron676@gmail.com>
2024-12-30 18:47:05 +0100

    chore: update sclc.user.js UserScript metadata

--- sclc.user.js
+++ sclc.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Steam Community Language Changer
-// @namespace    https://https://github.com/smooll-d
+// @namespace    https://github.com/smooll-d
 // @copyright    2024 Jakub Skowron
 // @version      1.0.0
 // @description  Changes language to preffered on Steam's community pages
@@ -8,6 +8,10 @@
 // @match        https://steamcommunity.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steamcommunity.com
 // @grant        none
+// @updateURL    https://github.com/smooll-d/sclc/raw/refs/heads/master/sclc.meta.js
+// @downloadURL  https://github.com/smooll-d/sclc/releases/latest/download/sclc.user.js
+// @supportURL   https://github.com/smooll-d/sclc/issues
+// @homepageURL  https://github.com/smooll-d/sclc
 // ==/UserScript==
 
 // Input your preffered language (full name in english, lowercase, e.g. english, polish, german, japanese)
