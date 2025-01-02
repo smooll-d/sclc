d583268dfe7c59f393241a33e110cd814275b1a6
Jakub Skowron <jakubskowron676@gmail.com>
2025-01-02 21:51:08 +0100

    feat(browser-language): add + test languages.json

--- sclc.meta.js
+++ sclc.meta.js
@@ -3,12 +3,13 @@
 // @namespace    https://github.com/smooll-d
 // @copyright    2024-2025, Jakub Skowron
 // @license      BSD-3-Clause
-// @version      1.0.3
+// @version      1.1.0
 // @description  Changes language to preffered on Steam's community pages
 // @author       smooll
 // @match        https://steamcommunity.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steamcommunity.com
-// @grant        none
+// @resource     languages https://https://github.com/smooll-d/sclc/raw/refs/heads/browser-language/languages.json
+// @grant        GM.getResourceText
 // @updateURL    https://github.com/smooll-d/sclc/raw/refs/heads/master/sclc.meta.js
 // @downloadURL  https://github.com/smooll-d/sclc/releases/latest/download/sclc.user.js
 // @supportURL   https://github.com/smooll-d/sclc/issues
